67f231029d9b6724fd9009ee4a49ca5d1bd55d09
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-06-23 23:45:14 -0300

    changed listen method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -16,6 +16,22 @@ const io = new Server(server, {
       origin: "http://localhost:3000"
     }
   });
+*/
+
+const express = require("express");
+const socketIO = require("socket.io");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 300;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
 
 io.on("connect", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -26,9 +42,9 @@ io.on("connect", (socket) => {
   });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log("Websocket server is running");
-});
+//server.listen(process.env.PORT, () => {
+//  console.log("Websocket server is running");
+//});
 
 //const teste = await db.query("SELECT * FROM users WHERE id = 6");
 
