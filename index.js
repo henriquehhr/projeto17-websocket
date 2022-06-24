6b72afa66ad5498c601123e83b6fbbe1cd42f26c
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-06-23 22:44:36 -0300

    added cors

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-import express, {json} from "express";
-import http from "http";
-import { Server } from "socket.io";
-import dotenv from "dotenv";
-
-import db from "./db.js";
-
+const express = require("express");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//import db from "./db.js";
+
+app.use(cors());
 dotenv.config();
 
 const server = http.createServer(app);
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000"
+    }
   });
 
+io.on("connect", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+    console.log("recebi uma mensagem: ", data);
+    io.emit("receive-message", data);
   });
 });
 
@@ -27,6 +30,6 @@ server.listen(process.env.PORT, () => {
   console.log("Websocket server is running");
 });
 
-const teste = await db.query("SELECT * FROM users WHERE id = 6");
+//const teste = await db.query("SELECT * FROM users WHERE id = 6");
 
-console.log(teste.rows[0].userName);
+//console.log(teste.rows[0].userName);
